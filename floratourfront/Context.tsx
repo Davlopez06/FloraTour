2950a70970d31b634ebaa02a677fe1a17f3d7f62
Davlopez06 <davidlopezvaldes@gmail.com>
2023-05-23 18:12:34 -0500

    Create Context & Routes

---
+++ floratourfront/Context.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+interface MyContextData {
+  value: string;
+  handleChangeValue: (newValue: string) => void;
+}
+
+interface MyComponentProps {
+    children: ReactNode;
+  }
+
+const MyContext = createContext<MyContextData | undefined>(undefined);
+
+const MyContextProvider: React.FC<MyComponentProps> = ({ children }) => {
+  const [value, setValue] = useState('');
+
+  const handleChangeValue = (newValue: string) => {
+    setValue(newValue);
+  };
+
+  const contextValues: MyContextData = {
+    value,
+    handleChangeValue,
+  };
+
+  return (
+    <MyContext.Provider value={contextValues}>
+      {children && React.Children.only(children)}
+    </MyContext.Provider>
+  );
+};
+
+const useMyContext = (): MyContextData => {
+  const context = useContext(MyContext);
+
+  if (!context) {
+    throw new Error('useMyContext must be used within a MyContextProvider');
+  }
+
+  return context;
+};
+
+export { MyContextProvider, useMyContext };
